a0294714b42488d65bcdbf16fca0a549d1f34cae
Zhong Chongshang <beatzcs@163.com>
2019-04-03 10:26:16 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,223 @@
+import data from "../../utils/data.js";
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    playStatus: true,
+    audioIndex: 0,
+    progress: 0,
+    duration: 0,
+    audioList: [],
+    showList: true
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this.setData({
+      audioList: data
+    })
+    this.playMusic();
+  },
+
+  playMusic: function() {
+    let audio = this.data.audioList[this.data.audioIndex];
+    let manager = wx.getBackgroundAudioManager();
+    manager.title = audio.name || "音频标题";
+    manager.epname = audio.epname || "专辑名称";
+    manager.singer = audio.author || "歌手名";
+    manager.coverImgUrl = audio.poster;
+    // 设置了 src 之后会自动播放
+    manager.src = audio.src;
+    manager.currentTime = 0;
+    let that = this;
+    manager.onPlay(function() {
+      console.log("======onPlay======");
+      that.setData({
+        playStatus: true
+      })
+      that.countTimeDown(that, manager);
+    });
+    manager.onPause(function() {
+      that.setData({
+        playStatus: false
+      })
+      console.log("======onPause======");
+    });
+    manager.onEnded(function() {
+      console.log("======onEnded======");
+      that.setData({
+        playStatus: false
+      })
+      setTimeout(function() {
+        that.nextMusic();
+      }, 1500);
+    });
+  },
+
+  //循环计时
+  countTimeDown: function(that, manager, cancel) {
+    if (that.data.playStatus) {
+      setTimeout(function() {
+        if (that.data.playStatus) {
+          // console.log("duration: " + manager.duration);
+          // console.log(manager.currentTime);
+          that.setData({
+            progress: Math.ceil(manager.currentTime),
+            progressText: that.formatTime(Math.ceil(manager.currentTime)),
+            duration: Math.ceil(manager.duration),
+            durationText: that.formatTime(Math.ceil(manager.duration))
+          })
+          that.countTimeDown(that, manager);
+        }
+      }, 1000)
+    }
+  },
+
+  //拖动事件
+  sliderChange: function(e) {
+    let manager = wx.getBackgroundAudioManager();
+    manager.pause();
+    manager.seek(e.detail.value);
+    this.setData({
+      progressText: this.formatTime(e.detail.value)
+    })
+    setTimeout(function() {
+      manager.play();
+    }, 1000);
+  },
+
+  //列表点击事件
+  listClick: function(e) {
+    let pos = e.currentTarget.dataset.pos;
+    if (pos != this.data.audioIndex) {
+      this.setData({
+        audioIndex: pos,
+        showList: false
+      })
+      this.playMusic();
+    } else {
+      this.setData({
+        showList: false
+      })
+    }
+  },
+
+  //上一首
+  lastMusic: function() {
+    let audioIndex = this.data.audioIndex > 0 ? this.data.audioIndex - 1 : this.data.audioList.length - 1;
+    this.setData({
+      audioIndex: audioIndex,
+      playStatus: false,
+      progress: 0,
+      progressText: "00:00",
+      durationText: "00:00"
+    })
+    setTimeout(function() {
+      this.playMusic();
+    }.bind(this), 1000);
+  },
+
+  //播放按钮
+  playOrpause: function() {
+    let manager = wx.getBackgroundAudioManager();
+    if (this.data.playStatus) {
+      manager.pause();
+    } else {
+      manager.play();
+    }
+  },
+
+  //下一首
+  nextMusic: function() {
+    let audioIndex = this.data.audioIndex < this.data.audioList.length - 1 ? this.data.audioIndex + 1 : 0;
+    this.setData({
+      audioIndex: audioIndex,
+      playStatus: false,
+      progress: 0,
+      progressText: "00:00",
+      durationText: "00:00"
+    })
+    setTimeout(function() {
+      this.playMusic();
+    }.bind(this), 1000);
+  },
+
+  //界面切换
+  pageChange: function() {
+    this.setData({
+      showList: true
+    })
+  },
+
+  //格式化时长
+  formatTime: function(s) {
+    let t = '';
+    s = Math.floor(s);
+    if (s > -1) {
+      let min = Math.floor(s / 60) % 60;
+      let sec = s % 60;
+      if (min < 10) {
+        t += "0";
+      }
+      t += min + ":";
+      if (sec < 10) {
+        t += "0";
+      }
+      t += sec;
+    }
+    return t;
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
---
+++ utils/data.js
@@ -0,0 +1,85 @@
+export default [{
+    src: 'http://static.missevan.com/MP3/201705/08/c8ab947c6a85092e5ffadcf02bbe348b184435.mp3',
+    poster: 'https://616e-anilagle-7474cd-1256663410.tcb.qcloud.la/logo部分的补充图/4.JPG?sign=62938ddecd979722c6726c100d8119fb&t=1547648207',
+    name: '青藏高原',
+    author: '韩红'
+  },
+  {
+    src: 'http://static.missevan.com/MP3/201705/07/5345b7a28e7734e3f1e773f007e79c00041356.mp3',
+    poster: 'https://616e-anilagle-7474cd-1256663410.tcb.qcloud.la/logo部分的补充图/4.JPG?sign=62938ddecd979722c6726c100d8119fb&t=1547648207',
+    name: '青花瓷',
+    author: '周杰伦'
+  },
+  {
+    src: 'http://static.missevan.com/MP3/201705/08/c8ab947c6a85092e5ffadcf02bbe348b184435.mp3',
+    poster: 'https://616e-anilagle-7474cd-1256663410.tcb.qcloud.la/logo部分的补充图/4.JPG?sign=62938ddecd979722c6726c100d8119fb&t=1547648207',
+    name: '饿狼传说',
+    author: '张靓颖'
+  },
+  {
+    src: 'http://static.missevan.com/MP3/201705/04/2b51fc04769a97cd12f520f33758d710221345.mp3',
+    poster: 'https://616e-anilagle-7474cd-1256663410.tcb.qcloud.la/logo部分的补充图/4.JPG?sign=62938ddecd979722c6726c100d8119fb&t=1547648207',
+    name: '海阔天空',
+    author: 'beyond'
+  },
+  {
+    src: 'http://static.missevan.com/MP3/201705/07/5345b7a28e7734e3f1e773f007e79c00041356.mp3',
+    poster: 'https://616e-anilagle-7474cd-1256663410.tcb.qcloud.la/补充图/61.png?sign=6999b5a45e683a2ee91d2976c1d0227c&t=1547713350',
+    name: '国王和乞丐',
+    author: "华晨宇",
+  },
+  {
+    src: 'http://static.missevan.com/MP3/201705/04/2b51fc04769a97cd12f520f33758d710221345.mp3',
+    poster: 'https://616e-anilagle-7474cd-1256663410.tcb.qcloud.la/补充图/61.png?sign=6999b5a45e683a2ee91d2976c1d0227c&t=1547713350',
+    name: '肩上蝶',
+    author: "金志文",
+  },
+  {
+    src: 'http://static.missevan.com/MP3/201705/07/5345b7a28e7734e3f1e773f007e79c00041356.mp3',
+    poster: 'https://616e-anilagle-7474cd-1256663410.tcb.qcloud.la/补充图/61.png?sign=6999b5a45e683a2ee91d2976c1d0227c&t=1547713350',
+    name: '爱笑的眼睛',
+    author: "林俊杰"
+  },
+  {
+    src: 'http://static.missevan.com/MP3/201705/08/c8ab947c6a85092e5ffadcf02bbe348b184435.mp3',
+    poster: 'https://616e-anilagle-7474cd-1256663410.tcb.qcloud.la/logo部分的补充图/4.JPG?sign=62938ddecd979722c6726c100d8119fb&t=1547648207',
+    name: '青藏高原',
+    author: '韩红'
+  },
+  {
+    src: 'http://static.missevan.com/MP3/201705/07/5345b7a28e7734e3f1e773f007e79c00041356.mp3',
+    poster: 'https://616e-anilagle-7474cd-1256663410.tcb.qcloud.la/logo部分的补充图/4.JPG?sign=62938ddecd979722c6726c100d8119fb&t=1547648207',
+    name: '青花瓷',
+    author: '周杰伦'
+  },
+  {
+    src: 'http://static.missevan.com/MP3/201705/08/c8ab947c6a85092e5ffadcf02bbe348b184435.mp3',
+    poster: 'https://616e-anilagle-7474cd-1256663410.tcb.qcloud.la/logo部分的补充图/4.JPG?sign=62938ddecd979722c6726c100d8119fb&t=1547648207',
+    name: '饿狼传说',
+    author: '张靓颖'
+  },
+  {
+    src: 'http://static.missevan.com/MP3/201705/04/2b51fc04769a97cd12f520f33758d710221345.mp3',
+    poster: 'https://616e-anilagle-7474cd-1256663410.tcb.qcloud.la/logo部分的补充图/4.JPG?sign=62938ddecd979722c6726c100d8119fb&t=1547648207',
+    name: '海阔天空',
+    author: 'beyond'
+  },
+  {
+    src: 'http://static.missevan.com/MP3/201705/07/5345b7a28e7734e3f1e773f007e79c00041356.mp3',
+    poster: 'https://616e-anilagle-7474cd-1256663410.tcb.qcloud.la/补充图/61.png?sign=6999b5a45e683a2ee91d2976c1d0227c&t=1547713350',
+    name: '国王和乞丐',
+    author: "华晨宇",
+  },
+  {
+    src: 'http://static.missevan.com/MP3/201705/04/2b51fc04769a97cd12f520f33758d710221345.mp3',
+    poster: 'https://616e-anilagle-7474cd-1256663410.tcb.qcloud.la/补充图/61.png?sign=6999b5a45e683a2ee91d2976c1d0227c&t=1547713350',
+    name: '肩上蝶',
+    author: "金志文",
+  },
+  {
+    src: 'http://static.missevan.com/MP3/201705/07/5345b7a28e7734e3f1e773f007e79c00041356.mp3',
+    poster: 'https://616e-anilagle-7474cd-1256663410.tcb.qcloud.la/补充图/61.png?sign=6999b5a45e683a2ee91d2976c1d0227c&t=1547713350',
+    name: '爱笑的眼睛',
+    author: "林俊杰"
+  }
+]
